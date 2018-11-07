0360a9ce7f0428971e98f09c8c3c059a8a5adcab
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-07 11:52:22 -0500

    adds express framework functionality

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+
 const User = require(`./models/User`);
 const Todo = require(`./models/Todo`);
 
+app.get('/', (req, res) => {
+  res.send('What up');
+})
 
-User.getById(2)
-  .then(result => {
-    result.getTodos()
-      .then(result => {
-        console.log(result)
-      })
-  })
-
-  .then(result => { console.log(result) })
+app.get('/allUsers', (req, res) => {
+  User.getAll()
+    .then(users => {
+      res.send(users);
+    })
+})
 
 
+app.listen(3000, () => {
+  console.log(`Server started...`);
+})
 
