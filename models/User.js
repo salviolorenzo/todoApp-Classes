90751e968834bfc6e725b494e45e61cad1209bee
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-07 10:40:38 -0500

    adds user class

--- models/User.js
+++ models/User.js
@@ -1,41 +1,120 @@
 const db = require('./db');
-//CREATE
-function addUser(name) {
-  return db.one(`insert into users(name)
-    values
-      ($1)
+
+class User {
+  constructor(id, name) {
+    this.id = id
+    this.name = name
+  }
+
+  // CREATE
+  static add(name) {
+    return db.one(`
+      insert into users(name)
+      values
+        ($1)
       returning id`,
-    [name]);
-}
+      [name])
+      .then(data => {
+        const u = new User(data.id, name);
+        return u;
+      })
+  }
+  // RETRIEVE
+  static getAll() {
+    return db.any(`
+    select * from users`)
+      .then(userArray => {
+        const instanceArray = userArray.map(userObj => {
+          const u = new User(userObj.id, userObj.name);
+          return u;
+        });
+        return instanceArray;
+      })
+  }
 
-// RETRIEVE
-function getAll() {
-  return db.any(`select * from users`);
-}
+  static getById(id) {
+    return db.one(`
+    select * from users
+    where id=$1`,
+      [id])
+      .then(result => {
+        const u = new User(result.id, result.name);
+        return u;
+      })
+  }
 
-function getUsersById(id) {
-  return db.any(`select * from users where id=$1`, [id]);
-}
+  static searchByName(name) {
+    return db.one(`
+    select * from users where name ilike %$1:raw%`, [name]);
+  }
 
-function getTodosForUser(id) {
-  return db.any(`
-    select * from todos
-      where user_id = $1`, [id]);
-}
-// UPDATE
-function updateName(id, name) {
-  return db.result(`
+  getTodos() {
+    return db.any(`
+    select * from todos where user_id = $1`, [this.id]);
+  }
+  // UPDATE
+  updateName(name) {
+    return db.result(`
     update users
       set name=$2
       where id=$1`,
-    [id, name]);
+      [this.id, name]);
+  }
+
+  // DELETE
+  deleteSelf() {
+    return db.result(`
+    delete from users where id=$1`,
+      [this.id]);
+  }
+
+  static deleteById(id) {
+    return db.result(`
+    delete from users where id =$1`,
+      [id]);
+  }
 }
 
-// DELETE
-function deleteById(id) {
-  return db.result(
-    `delete from users 
-    where id=$1`,
-    [id]
-  );
-}
+
+// WITHOUT CLASS
+
+// // CREATE
+// function addUser(name) {
+//   return db.one(`insert into users(name)
+//     values
+//       ($1)
+//       returning id`,
+//     [name]);
+// }
+
+// // RETRIEVE
+// function getAll() {
+//   return db.any(`select * from users`);
+// }
+
+// function getUsersById(id) {
+//   return db.any(`select * from users where id=$1`, [id]);
+// }
+
+// function getTodosForUser(id) {
+//   return db.any(`
+//     select * from todos
+//       where user_id = $1`, [id]);
+// }
+// // UPDATE
+// function updateName(id, name) {
+//   return db.result(`
+//     update users
+//       set name=$2
+//       where id=$1`,
+//     [id, name]);
+// }
+
+// // DELETE
+// function deleteById(id) {
+//   return db.result(
+//     `delete from users 
+//     where id=$1`,
+//     [id]
+//   );
+// }
